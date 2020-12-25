84f008df0f690ab6d3a760ad412832449590d6fa
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-25 15:09:07 -0800

    Fri Dec 25 15:09:08 PST 2020

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,28 +1,4 @@
 ((org-mode .  ((eval . (setq-local org-attach-id-dir  "~/Dropbox/org-notes/braindump/.attach/"))
-               (eval . (setq-local org-roam-directories
-                                   '(("Home roam" . "./")
-                                     ("GuangTao-private roam" . "./private-org/")
-                                     ("GuangTao-private-gtd roam" . "./private-org/GTD")
-                                     ("GuangTao roam" . "./GuangTao-owner")
-                                     ("gaming roam" . "./gaming")
-                                     ("life" . "./life")
-                                     ("financial" . "./financial")
-
-                                     ("programming roam" . "./programming")
-                                     ("emacs roam" . "./emacs")
-                                     ("math roam" . "./math")
-                                     ("data-science roam" . "./data-science")
-                                     ("system roam" . "./system")
-                                     ("hardware roam" . "./hardware")
-                                     ("deployment roam" . "./deployment")
-                                     ("network roam" . "./network")
-                                     ("nix roam" . "./nix")
-
-                                     ("security roam" . "./security")
-
-
-                                     ("contents roam" . "./pages"))
-                                   ))
                ;;(eval . (org-hugo-auto-export-mode))
                (eval . (setq-local wikinfo-base-url  "https://zh.wikipedia.org"))
                (eval . (setq-local org-roam-db-location "~/.emacs.d/.local/cache/org-roam-braindump.db"))
