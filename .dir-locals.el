8b7b11070b6cfc8382759f1c8bcc7c8a0017bcdf
GTrunSec <gtrunsec@hardenedlinux.org>
2020-12-25 00:02:03 -0800

    reorg

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,26 @@
 ((org-mode .  ((eval . (setq-local org-attach-id-dir  "~/Dropbox/org-notes/braindump/.attach/"))
                (eval . (setq-local org-roam-directories
                                    '(("Home roam" . "./")
-                                     ("private roam" . "./private-org/")
-                                     ("programming roam" . "./programming")
+                                     ("GuangTao-private roam" . "./private-org/")
+                                     ("GuangTao-private-gtd roam" . "./private-org/GTD")
                                      ("GuangTao roam" . "./GuangTao-owner")
+                                     ("gaming roam" . "./gaming")
                                      ("life" . "./life")
+                                     ("financial" . "./financial")
+
+                                     ("programming roam" . "./programming")
+                                     ("emacs roam" . "./emacs")
                                      ("math roam" . "./math")
+                                     ("data-science roam" . "./data-science")
+                                     ("system roam" . "./system")
+                                     ("hardware roam" . "./hardware")
+                                     ("deployment roam" . "./deployment")
+                                     ("network roam" . "./network")
+                                     ("nix roam" . "./nix")
+
+                                     ("security roam" . "./security")
+
+
                                      ("contents roam" . "./pages"))
                                    ))
                ;;(eval . (org-hugo-auto-export-mode))
